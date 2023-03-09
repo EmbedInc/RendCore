3d869fc68826a1cd4efec137c04c62323c3111a9
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2023-03-09 11:44:59 -0500

    Fixed installing of 2DIM CPNT and VECT callback routines.

--- rend_callback.pas
+++ rend_callback.pas
@@ -20,7 +20,7 @@ begin
   rend_callback.cpnt_2dim_call_p := routine_p; {save pointer to callback routine}
   rend_callback.cpnt_2dim_state_p := app_p; {save pointer to pass to callback}
 
-  rend_sw_set.cpnt_2dim :=             {install routine that will do the callback}
+  rend_set.cpnt_2dim :=                {install routine that will do the callback}
     addr(rend_sw_cpnt_2dim_cb);
   end;
 {
@@ -38,8 +38,8 @@ procedure rend_callback_vect_2dim (    {set app callback for PRIM.VECT_2DIM}
   val_param;
 
 begin
-  rend_callback.cpnt_2dim_call_p := routine_p; {save pointer to callback routine}
-  rend_callback.cpnt_2dim_state_p := app_p; {save pointer to pass to callback}
+  rend_callback.vect_2dim_call_p := routine_p; {save pointer to callback routine}
+  rend_callback.vect_2dim_state_p := app_p; {save pointer to pass to callback}
 
   rend_install_prim (                  {install routine that will do the callback}
     rend_sw_vect_2dim_cb_d, rend_prim.vect_2dim);
